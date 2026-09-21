7dcc0ef4c3af8652bf1548a9c33559e5864c8482
Alberto Giancarlo Ferrario <albertogferrario@gmail.com>
2026-09-21 05:21:28 +0200

    seo: keep Pages preview out of search indexes

---
+++ app/robots.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next';
+import { siteConfig } from '@/lib/site';
+
+export default function robots(): MetadataRoute.Robots {
+  const isPreview = process.env.GITHUB_PAGES === 'true';
+
+  if (isPreview) {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: '/',
+      },
+    };
+  }
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${siteConfig.url}/sitemap.xml`,
+    host: siteConfig.url,
+  };
+}
